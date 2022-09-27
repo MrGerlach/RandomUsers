4a7d814de2a257d1dac85fd97c5664eee9f5a41a
Mr. Gerlach <mgerl@tlen.pl>
2022-09-27 21:57:00 +0200

    App loads profile photo

--- RandomUsers/UsersView.swift
+++ RandomUsers/UsersView.swift
@@ -13,13 +13,20 @@ struct UsersView: View {
     var body: some View {
         NavigationView {
             List(userData.users) { user in
+                HStack {
+                    AsyncImage(url: URL(string: user.picture.thumbnail)) { image in
+                        image.clipShape(Circle())
+                    } placeholder: {
+                    Image(systemName: "person")
+                        .frame(width: 50, height: 50, alignment: .center)
+                }
                 Text(user.fullName)
         }
             .navigationTitle("Random Users")
         }
     }
 }
-
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         UsersView()
